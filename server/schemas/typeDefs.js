be259de23bba1f030a89cf58920561619208f5dc
aidan <aidanguarniere@gmail.com>
2021-03-16 14:29:18 -0700

    added query Thought and query Thought by username typeDefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,28 @@
 // import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
-// create our typeDefs
+// create typeDefs
 const typeDefs = gql`
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
   }
 `;
-
 // export the typeDefs
 module.exports = typeDefs;
